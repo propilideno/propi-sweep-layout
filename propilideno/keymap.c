d768a65c0f32d5bf351d1752dac2a771cd30e432
Lucas <lucas.r.almeida@edu.ufes.br>
2022-12-17 20:30:53 -0300

    swap slash by semicolon and spc by sft, bspc to hr

--- propilideno/keymap.c
+++ propilideno/keymap.c
@@ -407,29 +407,29 @@ Navigation layer (arrow keys and mouse control) - DEVELOPMENT
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [COLEMAK] = LAYOUT(
-    KC_Q, KC_W, KC_F, KC_P, KC_B,                                       KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
+    KC_Q, KC_W, KC_F, KC_P, KC_B,                                       KC_J, KC_L, KC_U, KC_Y, KC_BSPC,
     KC_A, KC_R, KC_S, KC_T, KC_G,                                       KC_M, KC_N, KC_E, KC_I, KC_O, 
-    KC_Z, LCTL_T(KC_X), LALT_T(KC_C), LGUI_T(KC_D), KC_V,               KC_K, LGUI_T(KC_H), LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
-                                MO(EXT) , LSFT_T(KC_SPC),               KC_SPC, MO(SYM)),
+    KC_Z, LCTL_T(KC_X), LALT_T(KC_C), LGUI_T(KC_D), KC_V,               KC_K, LGUI_T(KC_H), LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SCLN,
+                                        MO(EXT), KC_LSFT,               KC_SPC, MO(SYM)),
 
 
 [COLEMAK_MAC] = LAYOUT(
-    KC_Q, KC_W, KC_F, KC_P, KC_B,                                       KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
+    KC_Q, KC_W, KC_F, KC_P, KC_B,                                       KC_J, KC_L, KC_U, KC_Y, KC_BSPC,
     KC_A, KC_R, KC_S, KC_T, KC_G,                                       KC_M, KC_N, KC_E, KC_I, KC_O, 
-    KC_Z, LCTL_T(KC_X), LALT_T(KC_C), LGUI_T(KC_D), KC_V,               KC_K, LGUI_T(KC_H), LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
-                            MO(EXT_MAC) , LSFT_T(KC_SPC),               KC_SPC, MO(SYM)),
+    KC_Z, LCTL_T(KC_X), LALT_T(KC_C), LGUI_T(KC_D), KC_V,               KC_K, LGUI_T(KC_H), LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SCLN,
+                                    MO(EXT_MAC), KC_LSFT,               KC_SPC, MO(SYM)),
 
 [QWERTY] = LAYOUT(
     KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_Y, KC_U, KC_I, KC_O, KC_P,
     KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H, KC_J, KC_K, KC_L, KC_SCLN,
     KC_Z, KC_X, KC_C, KC_V, KC_B,                   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-        MO(EXT), LSFT_T(KC_SPC),                    KC_SPC, MO(SYM)),
+                MO(EXT), KC_LSFT,                   KC_SPC, MO(SYM)),
 
 [SYM] = LAYOUT(
     KC_1, KC_2, KC_3, KC_4, KC_5,                 KC_6, KC_7, KC_8, KC_9, KC_0, 
     KC_LT, KC_LPRN, KC_LBRC, KC_LCBR, KC_DLR,     KC_EQL, KC_QUES, KC_QUOT, KC_PLUS, KC_ASTR, 
     KC_GT, KC_RPRN, KC_RBRC, KC_RCBR, KC_PERC,    KC_AT, KC_EXLM, KC_DQUO, KC_MINS, KC_SLSH, 
-                          MO(FUNC), MO(SYM2),     KC_NO, KC_TRNS),
+                          MO(FUNC), MO(SYM2),     KC_SPC, KC_TRNS),
 
 [SYM2] = LAYOUT(
     KC_GRV, KC_TILDE, KC_CIRC, KC_HASH, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 
